b38d517c7d5c9e6561e759d029751eb263c6ef32
Leonardo Augusto <sleonardoaugusto@gmail.com>
2021-03-15 00:34:29 -0300

    Attendance viewset implemented

--- lavocat/api/v1/attendances/tests/test_views.py
+++ lavocat/api/v1/attendances/tests/test_views.py
@@ -1,14 +1,33 @@
 from django.test import TestCase
+from model_bakery import baker
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
+
+from lavocat.api.v1.attendances.serializers import AttendanceSerializer
+from lavocat.attendances.models import Attendance
 
 
 class Client:
     client = APIClient()
 
 
-class AttendanceViewsetTest(TestCase, Client):
+class AttendanceViewsetGetTest(TestCase, Client):
+    def setUp(self) -> None:
+        self.resp = self.client.get(reverse('api-v1:attendance-list'))
+
     def test_get(self):
-        resp = self.client.get(reverse('api-v1:attendance-list'))
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.resp.status_code, status.HTTP_200_OK)
+
+
+class AttendanceViewsetPostTest(TestCase, Client):
+    def setUp(self) -> None:
+        data = baker.prepare('Attendance')
+        payload = AttendanceSerializer(data).data
+        self.resp = self.client.post(
+            reverse('api-v1:attendance-list'), payload, content_type='application/json'
+        )
+
+    def test_post(self):
+        self.assertEqual(self.resp.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Attendance.objects.all().count(), 1)
