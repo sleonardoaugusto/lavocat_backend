d7fc048409cedd0d2ccc5bcec493dc66f9585df4
leonardoaugusto <sleonardoaugusto@gmail.com>
2022-12-13 20:54:23 -0300

    implement soft deletable models

--- lavocat/core/models.py
+++ lavocat/core/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
 
-class ModelBase(models.Model):
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at__isnull=True)
+
+
+class SoftDeleteModel(models.Model):
+    deleted_at = models.DateTimeField(null=True)
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+
+class ModelBase(SoftDeleteModel):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
